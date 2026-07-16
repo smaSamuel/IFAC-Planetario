29e791bad90f8d560545d0deb817badc72eba5b9
smaSamuel <samuel.oli@proton.me>
2026-07-16 10:21:07 -0500

    mudando a interface repository para uma classe abstract

--- src/repositories/UsuarioRepository.php
+++ src/repositories/UsuarioRepository.php
@@ -6,7 +6,7 @@
     use web\includes\Database;
     use web\Interfaces\Repository;
 
-        class UsuarioRepository implements Repository {
+        class UsuarioRepository extends Repository {
             private PDO $pdo;
 
             //Método __construct()
