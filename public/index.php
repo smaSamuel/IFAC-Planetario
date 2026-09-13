6559c5490d058031f12161fed81f8ab041e37671
Oliveira <sma.samuel.17@gmail.com>
2026-09-13 11:51:45 -0500

    rewrite

--- public/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once '../vendor/autoload.php'; //Carregando o autoload do composer
-ini_set('display_errors', 1); //Habilita e desabilida a visualização dos erros (1- ativo e 0- desativo)
